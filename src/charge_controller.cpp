bfecacd03b88d8e8fff37dfd31e95ae0bed73ef4
Wim426f <wim.boone@outlook.com>
2020-10-15 19:33:48 +0200

    master

--- src/charge_controller.cpp
+++ src/charge_controller.cpp
@@ -72,8 +72,8 @@ const char *ap_password = "3VLS042020";
 const char *ota_hostname = "FIORINO_ESP32";
 const char *ota_password = "MaPe1!";
 const char *dns_hostname = "fiorino";
-const char *http_username = "wim";
-const char *http_password = "w1mb00n3";
+const char *http_username = "3vls04";
+const char *http_password = "f!0r!n0";
 const char *PARAM_INPUT_1 = "function";
 
 IPAddress local_ip(192, 168, 1, 173);
