260c6da984988f47831127f5542b2d69743a4f31
Wim426f <wim.boone@outlook.com>
2020-11-12 10:16:26 +0100

    master

--- include/credentials.h
+++ include/credentials.h
@@ -2,15 +2,14 @@
 #define CREDENTIALS_H_
 
 /* Connectivity */
-const char *sta_ssid = "Boone-Huis";
-const char *sta_password = "b00n3meubelstof@";
+const char *sta_ssid = "";
+const char *sta_password = "";
 const char *ap_ssid = "Fiorino";
-const char *ap_password = "3VLS042020";
+const char *ap_password = "";
 const char *ota_hostname = "FIORINO_ESP32";
-const char *ota_password = "MaPe1!";
+const char *ota_password = "";
 const char *dns_hostname = "fiorino";
-const char *http_username = "3vls04";
-const char *http_password = "f!0r!n0";
-const char *PARAM_INPUT_1 = "function";
+const char *http_username = "";
+const char *http_password = "";
 
 #endif
